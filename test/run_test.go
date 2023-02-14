500d1e67d20e1cb7a414b234b012012d4861e40f
steden <1470804@qq.com>
2023-02-14 12:06:51 +0800

    修改测试用例

--- test/run_test.go
+++ test/run_test.go
@@ -3,10 +3,18 @@ package test
 import (
 	"github.com/farseer-go/fSchedule"
 	"github.com/farseer-go/fs"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
+	assert.NotPanics(t, func() {
+		fSchedule.Module{}.Shutdown()
+	})
 	fs.Initialize[fSchedule.Module]("test fSchedule")
-	fSchedule.Module{}.Shutdown()
+
+	client := fSchedule.GetClient()
+	client.LogoutClient()
+
+	//time.Sleep(1 * time.Hour)
 }
