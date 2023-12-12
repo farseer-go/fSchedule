325a525c15c99b03d207d7845d016e788fa5e2d5
steden <1470804@qq.com>
2023-12-12 21:53:36 +0800

    修改日志

--- healthCheck.go
+++ healthCheck.go
@@ -10,6 +10,12 @@ type healthCheck struct {
 
 func (c *healthCheck) Check() (string, error) {
 	err := defaultClient.RegistryClient()
-	tips := fmt.Sprintf("客户端(%d) %s:%d 注册成功！", defaultClient.ClientId, defaultClient.ClientIp, defaultClient.ClientPort)
+	var tips string
+	if err == nil {
+		tips = fmt.Sprintf("客户端(%d) %s:%d 注册成功！", defaultClient.ClientId, defaultClient.ClientIp, defaultClient.ClientPort)
+	} else {
+		tips = fmt.Sprintf("客户端(%d) %s:%d ", defaultClient.ClientId, defaultClient.ClientIp, defaultClient.ClientPort)
+	}
+
 	return "FSchedule." + strings.Join(defaultServer.Address, ",") + " " + tips, err
 }
