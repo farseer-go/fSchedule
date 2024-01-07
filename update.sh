39ca696c492eff7ba7f23ed561a1df85a9f52bfb
steden <1470804@qq.com>
2024-01-07 19:06:56 +0800

    移除文件

--- update.sh
+++
@@ -1,5 +0,0 @@
-go get github.com/farseer-go/collections
-go get github.com/farseer-go/fs
-go get github.com/farseer-go/utils
-go get github.com/farseer-go/webapi
-go get github.com/shirou/gopsutil/v3
